1fbff64f05a054ee729b9a2e31be1e2919847de0
sanghavikartigayan <sanghavi.kartigayan@performance.ca>
2021-01-12 14:04:29 -0500

    fixed composeEnhancer for async call

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,6 @@ import thunk from 'redux-thunk';
 import rootReducer from '../reducer';
 
 // For async code, advanced devtools setup
-const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+const composeEnhancers = (process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null) || compose;
 
 export default createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
